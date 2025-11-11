cc7f5412c2b3628c0ff8f66deb59796b4012a3b4
tohas1986 <tohas791@gmail.com>
2025-11-11 15:06:05 +0300

    Fix errors

--- src/components/Mixins/PasswordValidation.js
+++ src/components/Mixins/PasswordValidation.js
@@ -2,30 +2,14 @@ import { helpers } from 'vuelidate/lib/validators';
 
 export default {
   methods: {
-    minDigits(min) {
-      return helpers.withParams({ type: 'minDigits', min }, (value) => {
-        if (!value || typeof value !== 'string') return true;
-        return true;
-      });
+    minDigits() {
+      return true;
     },
-    minSpecialCharacters(min) {
-      return helpers.withParams(
-        { type: 'minSpecialCharacters', min },
-        (value) => {
-          if (!value || typeof value !== 'string') return true;
-            .length;
-          return true;
-        },
-      );
+    minSpecialCharacters() {
+      return true;
     },
     minUpperCaseCharacters(min) {
-      return helpers.withParams(
-        { type: 'minUpperCaseCharacters', min },
-        (value) => {
-          if (!value || typeof value !== 'string') return true;
-          return true;
-        },
-      );
+      return true;
     },
   },
 };
