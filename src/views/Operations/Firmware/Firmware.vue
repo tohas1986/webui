12e4af93d72088fa026ed241a7fef0bccf9cba56
tohas1986 <tohas791@gmail.com>
2025-11-24 14:22:18 +0300

    Fix

--- src/views/Operations/Firmware/Firmware.vue
+++ src/views/Operations/Firmware/Firmware.vue
@@ -65,8 +65,8 @@ import AlertsServerPower from './FirmwareAlertServerPower';
 import BmcCards from './FirmwareCardsBmc';
 import FormUpdate from './FirmwareFormUpdate';
 //import HostCards from './FirmwareCardsHost';
-//import PageSection from '@/components/Global/PageSection';
-//import PageTitle from '@/components/Global/PageTitle';
+import PageSection from '@/components/Global/PageSection';
+import PageTitle from '@/components/Global/PageTitle';
 import FirmwareUpdateStep from './FirmwareUpdateStep.vue';
 import FirmwareIstrueInfo from './FirmwareIstrueInfo.vue';
 import FirmwareUpdated from './FirmwareUpdated.vue';
@@ -78,7 +78,6 @@ export default {
     AlertsServerPower,
     BmcCards,
     FormUpdate,
-    HostCards,
     PageSection,
     PageTitle,
     FirmwareUpdateStep,
