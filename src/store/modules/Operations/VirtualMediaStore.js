7d65c23faedf1a70cb2c317cbeda0dbb8e103d6a
tohas1986 <tohas791@gmail.com>
2025-12-02 17:57:00 +0300

    Fixed VM Store

--- src/store/modules/Operations/VirtualMediaStore.js
+++ src/store/modules/Operations/VirtualMediaStore.js
@@ -23,15 +23,12 @@ const VirtualMediaStore = {
   getters: {
     proxyDevices: (state) => state.proxyDevices,
     legacyDevices: (state) => state.legacyDevices,
-    connections: (state) => state.connections,
   },
   mutations: {
     setProxyDevicesData: (state, deviceData) =>
       (state.proxyDevices = deviceData),
     setLegacyDevicesData: (state, deviceData) =>
       (state.legacyDevices = deviceData),
-    setConnectionsData: (state, connections) =>
-      (state.connections = connections),
   },
   actions: {
     async getData({ commit }) {
@@ -54,26 +51,20 @@ const VirtualMediaStore = {
       return await api
         .get('/redfish/v1/Managers/bmc/VirtualMedia')
         .then((response) =>
-          response.data.Members.map((virtualMedia) => virtualMedia['@odata.id'])
+          response.data.Members.map(
+            (virtualMedia) => virtualMedia['@odata.id'],
+          ),
         )
         .then((devices) => api.all(devices.map((device) => api.get(device))))
         .then((devices) => {
           const deviceData = devices.map((device) => {
             const isActive = device.data?.Inserted === true ? true : false;
-            let file = null;
-            if (isActive === true) {
-              file = new File([], device.data?.FilePath);
-            }
-            const username = device.data?.UserName;
-            const serverUri = device.data?.FilePath;
             return {
               id: device.data?.Id,
               transferProtocolType: device.data?.TransferProtocolType,
-              websocket: device.data?.Oem?.OpenBMC?.WebSocketEndpoint,
+              websocket: device.data?.Oem?.OpenYard?.WebSocketEndpoint,
+              imageName: device.data?.ImageName,
               isActive: isActive,
-              file,
-              username,
-              serverUri,
             };
           });
           const proxyDevices = deviceData
@@ -81,15 +72,18 @@ const VirtualMediaStore = {
             .map((device) => {
               return {
                 ...device,
+                file: null,
               };
             });
           const legacyDevices = deviceData
             .filter((d) => d.transferProtocolType !== transferProtocolType.OEM)
             .map((device) => {
               return {
-                ...parseServerUri(device.serverUri),
                 ...device,
+                serverUri: '',
+                username: '',
                 password: '',
+                isCD: false,
                 isRW: false,
               };
             });
@@ -104,7 +98,7 @@ const VirtualMediaStore = {
       return await api
         .post(
           `/redfish/v1/Managers/bmc/VirtualMedia/${id}/Actions/VirtualMedia.InsertMedia`,
-          data
+          data,
         )
         .catch((error) => {
           console.log('Mount image:', error);
@@ -114,79 +108,15 @@ const VirtualMediaStore = {
     async unmountImage(_, id) {
       return await api
         .post(
-          `/redfish/v1/Managers/bmc/VirtualMedia/${id}/Actions/VirtualMedia.EjectMedia`
+          `/redfish/v1/Managers/bmc/VirtualMedia/${id}/Actions/VirtualMedia.EjectMedia`,
         )
         .catch((error) => {
           console.log('Unmount image:', error);
           throw new Error();
         });
     },
-    async uploadFilename(_, { id, filename }) {
-      try {
-        const { data } = await api.get('/redfish/v1/Managers/bmc/VirtualMedia');
-        const slotArr = data.Members.map((vm) => vm['@odata.id']);
-        const apiUrl = slotArr.filter((slot) => slot.includes(id))[0];
-        const result = await api.post(apiUrl, { FilePath: filename });
-        return result;
-      } catch (error) {
-        console.log(error);
-        throw error;
-      }
-    },
-    async unmountLocalImage({ dispatch }, slotId) {
-      try {
-        const id = slotId.slice(-1);
-        const { data } = await api.post(
-          `/redfish/v1/Managers/bmc/VirtualMedia/close/${id}`
-        );
-        if (data?.status === 'success') {
-          await dispatch('uploadFilename', {
-            id: slotId,
-            filename: '',
-          });
-        }
-      } catch (error) {
-        console.log(error);
-        throw error;
-      }
-    },
-    async getLocalImageStatus(_, id) {
-      try {
-        const { data } = await api.get('/redfish/v1/Managers/bmc/VirtualMedia');
-        const slotArr = data.Members.map((vm) => vm['@odata.id']);
-        const apiUrl = slotArr.filter((slot) => slot.includes(id))[0];
-        const result = await api.get(apiUrl);
-        const isActive = result?.data?.Inserted;
-        return isActive;
-      } catch (error) {
-        console.log(error);
-      }
-    },
   },
 };
 
-function parseServerUri(uri) {
-  let protocol = null,
-    server = null,
-    filePath = null;
-  const nfsRegex = /nfs:\/\/(.*?):(\/.+)/;
-  const smbRegex = /smb:\/\/(.*?)(\/.*)/;
-  if (uri.startsWith('nfs://')) {
-    protocol = 'nfs://';
-    const match = uri.match(nfsRegex);
-    if (match) {
-      server = match[1];
-      filePath = match[2];
-    }
-  } else if (uri.startsWith('smb://')) {
-    protocol = 'smb://';
-    const match = uri.match(smbRegex);
-    if (match) {
-      server = match[1];
-      filePath = match[2];
-    }
-  }
-  return { protocol, server, filePath };
-}
-
 export default VirtualMediaStore;
+
