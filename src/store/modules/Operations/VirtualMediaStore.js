561dbf457e8fb9b8506e0938a2a786b850915f76
tohas1986 <tohas791@gmail.com>
2025-12-02 18:19:35 +0300

    Fix

--- src/store/modules/Operations/VirtualMediaStore.js
+++ src/store/modules/Operations/VirtualMediaStore.js
@@ -50,9 +50,7 @@ const VirtualMediaStore = {
 
       return await api
         .get('/redfish/v1/Managers/bmc/VirtualMedia')
-        .then((response) =>
-          response.data.Members.map(
-            (virtualMedia) => virtualMedia['@odata.id'],
+        .then((response) => response.data.Members.map((virtualMedia) => virtualMedia['@odata.id'],
           ),
         )
         .then((devices) => api.all(devices.map((device) => api.get(device))))
@@ -98,7 +96,7 @@ const VirtualMediaStore = {
       return await api
         .post(
           `/redfish/v1/Managers/bmc/VirtualMedia/${id}/Actions/VirtualMedia.InsertMedia`,
-          data,
+          data
         )
         .catch((error) => {
           console.log('Mount image:', error);
@@ -108,7 +106,7 @@ const VirtualMediaStore = {
     async unmountImage(_, id) {
       return await api
         .post(
-          `/redfish/v1/Managers/bmc/VirtualMedia/${id}/Actions/VirtualMedia.EjectMedia`,
+          `/redfish/v1/Managers/bmc/VirtualMedia/${id}/Actions/VirtualMedia.EjectMedia`
         )
         .catch((error) => {
           console.log('Unmount image:', error);
@@ -119,4 +117,3 @@ const VirtualMediaStore = {
 };
 
 export default VirtualMediaStore;
-
