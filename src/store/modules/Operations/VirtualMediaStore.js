7475c75549552e9f28efa99c46297ddef2f66edc
tohas1986 <tohas791@gmail.com>
2025-12-02 18:37:17 +0300

    Fix

--- src/store/modules/Operations/VirtualMediaStore.js
+++ src/store/modules/Operations/VirtualMediaStore.js
@@ -51,7 +51,7 @@ const VirtualMediaStore = {
       return await api
         .get('/redfish/v1/Managers/bmc/VirtualMedia')
         .then((response) => response.data.Members.map((virtualMedia) => virtualMedia['@odata.id'])
-      )
+        )
         .then((devices) => api.all(devices.map((device) => api.get(device))))
         .then((devices) => {
           const deviceData = devices.map((device) => {
