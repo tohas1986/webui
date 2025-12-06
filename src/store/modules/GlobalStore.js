20f2f462cf09a961b36283e92801bd23cb014469
tohas1986 <tohas791@gmail.com>
2025-12-06 20:45:26 +0300

    Fixes

--- src/store/modules/GlobalStore.js
+++ src/store/modules/GlobalStore.js
@@ -114,15 +114,13 @@ const GlobalStore = {
     getServerInfo({ commit }) {
       api
         .get('/redfish/v1/Chassis/BMC_FRU')
-        .then(
-          ({ data: { Model, Manufacturer, SerialNumber }
-          } = {}) => {
+        .then(({ data: { Model, Manufacturer, SerialNumber }} = {}) => {
           commit('setAssetTag', SerialNumber);
           commit('setSerialNumber', SerialNumber);
           commit('setModelType', Model);
           if (Manufacturer === ' ') { 
             commit('setMfg', 'RAMEC');
-          } else { 
+          } else {
             commit('setMfg', Manufacturer);
           }
         }
