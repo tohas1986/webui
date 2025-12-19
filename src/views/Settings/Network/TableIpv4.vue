afc3c15071aee30668ea22974983cda24f11b8e8
tohas1986 <tohas791@gmail.com>
2025-12-19 18:14:10 +0300

    Some fixes

--- src/views/Settings/Network/TableIpv4.vue
+++ src/views/Settings/Network/TableIpv4.vue
@@ -229,7 +229,7 @@ export default {
               : this.$t('global.action.disable'),
             okVariant: 'danger',
             cancelTitle: this.$t('global.action.cancel'),
-          },
+          }
         )
         .then((dhcpEnableConfirmed) => {
           if (dhcpEnableConfirmed) {
