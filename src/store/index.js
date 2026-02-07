ac4fb851b44c0fa34c600221d0b723cd2ce20e38
tohas1986 <tohas791@gmail.com>
2026-02-07 12:50:47 +0300

    Fix error

--- src/store/index.js
+++ src/store/index.js
@@ -32,6 +32,7 @@ import KeyClearStore from './modules/Operations/KeyClearStore';
 import WebSocketPlugin from './plugins/WebSocketPlugin';
 import DateTimeStore from './modules/Settings/DateTimeStore';
 import VirtualMediaStore from './modules/Operations/VirtualMediaStore';
+import ThermalControlStore from './modules/Settings/ThermalControl';
 
 Vue.use(Vuex);
 
@@ -69,6 +70,7 @@ export default new Vuex.Store({
     policies: PoliciesStore,
     factoryReset: FactoryResetStore,
     keyClear: KeyClearStore,
+    thermal: ThermalControlStore,
   },
   plugins: [WebSocketPlugin],
 });
