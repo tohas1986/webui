e577ec4b18d4d672e3aad40e086a56b90395c8db
tohas1986 <tohas791@gmail.com>
2026-02-10 17:37:05 +0300

    Error

--- src/store/modules/Settings/ThermalControl.js
+++ src/store/modules/Settings/ThermalControl.js
@@ -68,8 +68,8 @@ const ThermalControlStore = {
             if (fanType === 'Speed') {
               // Извлекаем направление (F/R) из MemberId или Name
               const directionMatch = 
-              fan.MemberId?.match(/FAN\d+_([FR])_Speed/) ||
-              fan.Name.match(/FAN\d+\s+([FR])\s+Speed/);
+                fan.MemberId?.match(/FAN\d+_([FR])_Speed/) ||
+                fan.Name.match(/FAN\d+\s+([FR])\s+Speed/);
               if (directionMatch) {
                 fanInfo.location = `${this.getFanLocation(fanNumber)} (${
                   directionMatch[1]
