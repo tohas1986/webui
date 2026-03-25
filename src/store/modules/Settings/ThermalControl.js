9c8c50a891dc3ab29e4884c350e3697ebf2b5770
tohas1986 <tohas791@gmail.com>
2026-03-25 22:14:27 +0300

    Fix thermal

--- src/store/modules/Settings/ThermalControl.js
+++ src/store/modules/Settings/ThermalControl.js
@@ -60,7 +60,9 @@ const ThermalControlStore = {
           const fanMap = new Map();
           // Группировка данных по вентиляторам
           fans.forEach((fan) => {
-            const nameMatch = fan.Name.match(/FAN(\d+)\s+([FR])\s+Speed/) || fan.Name.match(/FAN(\d+)\s+(PWM)/);
+            const nameMatch = 
+              fan.Name.match(/FAN(\d+)\s+([FR])\s+Speed/) || 
+              fan.Name.match(/FAN(\d+)\s+(PWM)/);
             if (!nameMatch) return;
             const fanNumber = nameMatch[1];
             const fanType = nameMatch[2];
